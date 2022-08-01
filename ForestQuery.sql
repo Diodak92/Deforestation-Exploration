576db8bd3a142b2aa16752384db27f9da23c570c
Diodak92 <tomasz.kowalski92@icloud.com>
2022-08-01 23:39:56 +0200

    Join between land and forest tables

--- ForestQuery.sql
+++ ForestQuery.sql
@@ -1,4 +1,10 @@
--- convert land area to square kilimeters
-SELECT country_code, year,
-total_area_sq_mi * 2.59 AS total_area_sqkm
-FROM land_area
+-- join land and forest tables
+SELECT *
+FROM forest_area AS fa
+JOIN(
+    -- convert land area to square kilimeters
+    SELECT country_code, year,
+    total_area_sq_mi * 2.59 AS total_area_sqkm
+    FROM land_area) AS la
+ON fa.country_code = la.country_code
+AND fa.year = la.year
