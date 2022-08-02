ba9dbf5689bbfaaa60d4d79f8099cde318755945
Diodak92 <tomasz.kowalski92@icloud.com>
2022-08-02 15:23:55 +0200

    Added solution for a global situation

--- ForestQuery.sql
+++ ForestQuery.sql
@@ -16,3 +16,80 @@ AND fa.year = la.year
 JOIN regions AS r
 ON fa.country_code = r.country_code
 ORDER BY country_code, year;
+
+-- PART 2 
+-- GLOBAL SITUATION
+
+-- Q1
+/* What was the total forest area (in sq km) of the world in 1990?
+Please keep in mind that you can use the country record denoted as “World" in the region table.*/
+
+SELECT SUM(forest_area_sqkm) AS total_forest_area_1990
+FROM forestation
+WHERE year = 1990;
+
+-- Q2
+/* What was the total forest area (in sq km) of the world in 2016?
+ Please keep in mind that you can use the country record in the table is denoted as “World.” */
+
+SELECT SUM(forest_area_sqkm) AS total_forest_area_2016
+FROM forestation
+WHERE year = 2016;
+
+-- Q3
+/* What was the change (in sq km) in the forest area of the world from 1990 to 2016? */
+
+WITH table1 AS(
+  SELECT SUM(forest_area_sqkm) AS total_forest_area_1990
+  FROM forestation
+  WHERE year = 1990),
+
+table2 AS(
+  SELECT SUM(forest_area_sqkm) AS total_forest_area_2016
+  FROM forestation
+  WHERE year = 2016)
+
+SELECT table1.total_forest_area_1990 - table2.total_forest_area_2016 AS forest_area_change
+FROM table1, table2;
+
+
+-- Q4
+/* What was the percent change in forest area of the world between 1990 and 2016? */
+
+WITH table1 AS(
+  SELECT SUM(forest_area_sqkm) AS total_forest_area_1990
+  FROM forestation
+  WHERE year = 1990),
+
+table2 AS(
+  SELECT SUM(forest_area_sqkm) AS total_forest_area_2016
+  FROM forestation
+  WHERE year = 2016)
+
+SELECT ROUND((((table1.total_forest_area_1990 - table2.total_forest_area_2016) /
+table1.total_forest_area_1990) * 100) :: NUMERIC, 2) :: VARCHAR || '%'
+AS forest_area_change
+FROM table1, table2;
+
+-- Q5
+/* If you compare the amount of forest area lost between 1990 and 2016,
+to which country's total area in 2016 is it closest to? */
+
+SELECT DISTINCT country_name, total_area_sqkm
+FROM forestation
+WHERE total_area_sqkm >= (
+  WITH table1 AS(
+    SELECT SUM(forest_area_sqkm) AS total_forest_area_1990
+    FROM forestation
+    WHERE year = 1990),
+
+  table2 AS(
+    SELECT SUM(forest_area_sqkm) AS total_forest_area_2016
+    FROM forestation
+    WHERE year = 2016)
+
+  SELECT table1.total_forest_area_1990 - table2.total_forest_area_2016 AS forest_area_change
+  FROM table1, table2)
+ORDER BY total_area_sqkm
+LIMIT 1;
+
--- ProjectTasks.md
+++ ProjectTasks.md
@@ -1,4 +1,4 @@
-# Steps to Complete
+## PART 1 - JOIN TABLES AND CREATE VIEW
 
 1. Create a View called “forestation” by joining all three tables - forest_area, land_area and regions in the workspace.
 2. The forest_area and land_area tables join on both country_code AND year.
@@ -8,3 +8,11 @@
     * A new column that provides the percent of the land area that is designated as forest.
 
 5. Keep in mind that the column forest_area_sqkm in the forest_area table and the land_area_sqmi in the land_area table are in different units (square kilometers and square miles, respectively), so an adjustment will need to be made in the calculation you write (1 sq mi = 2.59 sq km).
+
+## PART 2 - GLOBAL SITUATION
+
+1. What was the total forest area (in sq km) of the world in 1990? Please keep in mind that you can use the country record denoted as “World" in the region table.
+2. What was the total forest area (in sq km) of the world in 2016? Please keep in mind that you can use the country record in the table is denoted as “World.”
+3. What was the change (in sq km) in the forest area of the world from 1990 to 2016?
+4. What was the percent change in forest area of the world between 1990 and 2016?
+5.  If you compare the amount of forest area lost between 1990 and 2016, to which country's total area in 2016 is it closest to?
