ff029ba47429e1ce4142c9834f67df440397b057
Diodak92 <tomasz.kowalski92@icloud.com>
2022-08-06 17:09:10 +0200

    Create land area, forest area and regions tables

--- ForestDDL.sql
+++ ForestDDL.sql
@@ -1,3 +1,21 @@
-CREATE TABLE ""(
-    
-)
+-- create forest area table
+CREATE TABLE IF NOT EXIST "forest_area"(
+    "country_code" VARCHAR,
+    "country_name" VARCHAR,
+    "year" SMALLINT,
+    "forest_area_sqkm" FLOAT8
+);
+-- create land area table
+CREATE TABLE IF NOT EXIST "forest_area"(
+    "country_code" VARCHAR,
+    "country_name" VARCHAR,
+    "year" SMALLINT,
+    "total_area_sq_mi" FLOAT8
+);
+-- create regions table
+CREATE TABLE IF NOT EXIST "forest_area"(
+    "country_name" VARCHAR,
+    "country_code" VARCHAR,
+    "region" VARCHAR,
+    "income_group" FLOAT8
+);
