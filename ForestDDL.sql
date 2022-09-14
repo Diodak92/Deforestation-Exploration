1d8a81c48c3bb6dbd4aa51244e49f1e3d05784bf
Diodak92 <tomasz.kowalski92@icloud.com>
2022-09-14 22:48:50 +0200

    Fixed table definition

--- ForestDDL.sql
+++ ForestDDL.sql
@@ -1,21 +1,24 @@
+-- create new database
+CREATE DATABASE deforestation; 
+
 -- create forest area table
-CREATE TABLE IF NOT EXIST "forest_area"(
+CREATE TABLE IF NOT EXISTS "forest_area"(
     "country_code" VARCHAR,
     "country_name" VARCHAR,
     "year" SMALLINT,
     "forest_area_sqkm" FLOAT8
 );
 -- create land area table
-CREATE TABLE IF NOT EXIST "forest_area"(
+CREATE TABLE IF NOT EXISTS "land_area"(
     "country_code" VARCHAR,
     "country_name" VARCHAR,
     "year" SMALLINT,
     "total_area_sq_mi" FLOAT8
 );
 -- create regions table
-CREATE TABLE IF NOT EXIST "forest_area"(
+CREATE TABLE IF NOT EXISTS "regions"(
     "country_name" VARCHAR,
     "country_code" VARCHAR,
     "region" VARCHAR,
-    "income_group" FLOAT8
+    "income_group" VARCHAR
 );
