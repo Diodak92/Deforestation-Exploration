5d4ad274fc06e404987429e0f9592a8aa79a874f
Diodak92 <tomasz.kowalski92@icloud.com>
2022-09-20 12:37:45 +0200

    Create deforestation VIEW move into ForestDDL.sql file

--- ForestDDL.sql
+++ ForestDDL.sql
@@ -1,6 +1,3 @@
--- create new database
-CREATE DATABASE deforestation; 
-
 -- create forest area table
 CREATE TABLE IF NOT EXISTS "forest_area"(
     "country_code" VARCHAR,
@@ -42,3 +39,24 @@ COPY regions
 FROM '/absolute_path/Deforestation-Exploration/db_files/regions.csv'
 DELIMITER ','
 CSV HEADER;
+
+-- Create a View by joining all three tables - forest_area, land_area and regions
+CREATE VIEW forestation AS
+SELECT fa.country_code,
+  fa.country_name,
+  r.region,
+  r.income_group,
+  fa.year,
+  fa.forest_area_sqkm,
+  -- convert land area to square kilimeters
+  la.total_area_sq_mi * 2.59 AS total_area_sqkm,
+  -- compute the percent of forest area
+  (
+    fa.forest_area_sqkm / (la.total_area_sq_mi * 2.59)
+  ) * 100 AS forest_percent
+FROM forest_area AS fa -- join land and forest tables
+  JOIN land_area AS la ON fa.country_code = la.country_code
+  AND fa.year = la.year -- join region to forest table
+  JOIN regions AS r ON fa.country_code = r.country_code
+ORDER BY country_code,
+  year;
